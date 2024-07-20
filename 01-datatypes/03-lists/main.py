bc96157e3042260e372299fc4475d8e3d2baf38d
Bernard Dev <devwithbernard@gmail.com>
2024-07-20 12:18:20 +0000

    List in python

---
+++ 01-datatypes/03-lists/main.py
@@ -0,0 +1,26 @@
+"""
+A list of numbers
+"""
+numbers = [1, 2, 3, 4, 5]
+
+print(type(numbers), '--> ', numbers)
+if isinstance(numbers, list):
+    index = 0
+    for index, number in enumerate(numbers):
+        print(f"{index} -> {number}")
+
+"""
+List indexing : ['a', 'b', 'c', 'd']
+                  0    1    2    3   : positive indexation
+                  -4   -3   -2   -1  : negative indexation
+"""
+
+products = ['Shoe', 'Trowser', 'Bag', 'Watch']
+for idx in range(len(products)): # Positive indexation
+    product = products[idx]
+    print(f"Product N°{idx + 1} --> {product}")
+
+for idx in range(1, len(products) + 1): # Negative indexation
+    product = products[-idx]
+    print(f"Product N°{len(products) + 1 - idx} --> {product}")
+
