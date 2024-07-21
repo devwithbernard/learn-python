8ea271ffa2fbffd9ce667d12a14a540a92fad654
Bernard Dev <devwithbernard@gmail.com>
2024-07-21 12:35:59 +0000

    More about python dictionaries

--- 01-datatypes/05-dictionaries/main.py
+++ 01-datatypes/05-dictionaries/main.py
@@ -7,11 +7,39 @@ student = {
     'courses': ["Math", 'Computer science', 'Biology']
 }
 
-print(student)
+# print(student)
+#
+# # Access value
+# print('Name of student: ', student['name'])
+#
+# if student.get('phone') is None:
+#     student['phone'] = '+01-185-128-855-326'
+#
+# student['name'] = 'John'
+#
+# # update key values
+# student.update({"name": 'Jack Doe', 'phone': '+225-07-694-543-35'})
+#
+# print(student)
+#
+# # delete key
+# del student['age']
+#
+# print("New student infos: ", student)
 
-# Access value
-print('Name of student: ', student['name'])
+# Number of keys
+print('number of keys: ', len(student))
 
-if student.get('phone') is None:
-    student['phone'] = '+01-185-128-855-326'
-print(student)
+# print student keys, values
+print(student.keys())
+print(student.values())
+
+# keys and values
+print(student.items())
+
+# Loop through dictionary
+for key in student:
+    print(key)
+
+for key, value in student.items():
+    print(key, value)
