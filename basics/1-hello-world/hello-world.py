8d9de7e9f5071201e3407df24b7ca34fe682b7b9
Bernard Dev <devwithbernard@gmail.com>
2024-08-02 16:31:33 +0000

    first Hello World program

---
+++ basics/1-hello-world/hello-world.py
@@ -0,0 +1,14 @@
+#! usr/bin/python3
+
+"""
+Print starting phrase : Hello World
+"""
+
+print('Hello World')
+
+
+# Another way
+from sys import stdout
+
+message = 'Hello world Everyone!'
+stdout.write(message)
