879a35f10d6552bd03cfc7adcb83fc4943a2888b
= <=>
2024-08-05 17:51:19 +0000

    String declaration

---
+++ basics/3-datatypes/datatypes.py
@@ -0,0 +1,11 @@
+"""
+ Datatypes in python
+"""
+
+
+if __name__ == '__main__':
+    # String
+    welcome: str = 'Welcome to Japan'
+
+    if isinstance(welcome, str):
+        print(f"'{welcome}' is a string.")
