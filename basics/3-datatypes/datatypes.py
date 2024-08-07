b7205c6788f12de82ac30cf56de2cbf4e5c78a18
= <=>
2024-08-07 14:56:50 +0000

    String slicing

--- basics/3-datatypes/datatypes.py
+++ basics/3-datatypes/datatypes.py
@@ -9,3 +9,7 @@ if __name__ == '__main__':
 
     if isinstance(welcome, str):
         print(f"'{welcome}' is a string.")
+
+    # Slicing a string
+    five_first_characters = welcome[:5]
+    print(f"five first characters of '{welcome}' => '{five_first_characters}'")
