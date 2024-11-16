8203f8bb49875e4246f4df5052be9a6f5f1ab69b
= <=>
2024-11-16 21:28:50 +0000

    Create a list

---
+++ 5-lists/main.py
@@ -0,0 +1,11 @@
+
+def main() -> None:
+    # Create a list
+
+    fruits: list[str] = ["Apple", "Banana", "Cherry"]
+    print(list, "Type: ", type(fruits))
+
+
+if __name__ == '__main__':
+    main()
+
