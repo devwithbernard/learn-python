2366c52d15ea532ba226da5da665204b02b47558
= <=>
2024-11-18 16:37:54 +0000

    Access List Items: Range of Indexes

--- 5-lists/accessing-items/main.py
+++ 5-lists/accessing-items/main.py
@@ -13,7 +13,7 @@ def main() -> None:
         """
         if len(iterable) > 2:
             first_item: str = iterable[-len(iterable)]
-            second_item: str = iterable[1-len(iterable)]
+            second_item: str = iterable[1 - len(iterable)]
             last_item: str = iterable[-1]
             print(f"""
             Negative indexing:
@@ -22,7 +22,25 @@ def main() -> None:
               last_item => {last_item}
             """)
 
+    def range_of_indexes(iterable: list[str], max_range: int, min_range: int = 0) -> None:
+        """
+        Range of indexes
+        :param iterable: list of items
+        :param min_range: min of range while indexing
+        :param max_range: max of range while indexing
+        """
+        range_of_iterable: list[str] = []
+        if (0 <= min_range < max_range) and (max_range <= len(iterable) + 1):
+            range_of_iterable = iterable[min_range: max_range]
+        if max_range > len(iterable) + 1:
+            range_of_iterable = iterable[min_range:]
+        else:
+            print("Verify arguments!\nmax_range must be greater than min_range")
+
+        print(f"Range of indexes: {range_of_iterable}")
+
     negative_indexing(fruits)
+    range_of_indexes(fruits, 10, 2)
 
 
 if __name__ == '__main__':
