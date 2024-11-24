1aa6ca41ee16d99f05acee0e2d52a574370ed8c4
= <=>
2024-11-24 20:50:41 +0000

    Change a range of item values

--- 5-lists/change-items/main.py
+++ 5-lists/change-items/main.py
@@ -1,6 +1,7 @@
 """
 Change List Items
 """
+from copy import copy
 from typing import Sequence, Any
 
 
@@ -20,7 +21,30 @@ def main() -> None:
         else:
             print(f"{old_value} isn't ")
 
+    def change_range_of_item_values(
+            iterable: Sequence, start_index: int,
+            end_index: int, range_of_items) -> None:
+        """
+        Change a range of items
+        :param iterable: An iterable
+        :param start_index: started index of replaced range
+        :param end_index: ended index of replaced range
+        :param range_of_items: range of items to replace
+        """
+        duplicated_iterable: Sequence = copy(iterable)
+        if start_index <= end_index <= len(iterable):
+            duplicated_iterable[start_index:end_index] = range_of_items
+
+            print(f"""
+                Last Sequence => {iterable}
+                New Sequence => {duplicated_iterable}
+            """)
+        else:
+            print("Error of indexes")
+            raise IndexError("Started Index or Ended index must be ordered!")
+
     change_item_value(fruits, 'Mango', 'Cherry')
+    change_range_of_item_values(fruits, 1, 3, ['Melon', 'Orange'])
 
 
 if __name__ == '__main__':
