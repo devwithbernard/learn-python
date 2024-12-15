24b250054950e2406deb898f91fcf45cf6fbbb8d
Bernard Konan <devwithbernard@gmail.com>
2024-12-15 22:47:50 +0000

    Loop through the index numbers

--- 5-lists/loop-lists/main.py
+++ 5-lists/loop-lists/main.py
@@ -26,12 +26,25 @@ def loop_through_list(sequence: List[dict]) -> None:
              title: {item["title"]}
              completed: {item["completed"]}
         """)
-        print("-"*100)
+        print("-" * 100)
+
+
+def loop_through_index_numbers(sequence: List[dict]) -> None:
+    for index in range((len(sequence))):
+        if index % 2 == 0 and index <= 10:
+            item: dict = sequence[index]
+            print(f"""
+            Todo(id={item['id']}):
+              title: {item["title"]}
+              completed: {item["completed"]}  
+            """)
+            print("-" * 50)
 
 
 def main() -> None:
     todos: List[dict] = get_todos("https://jsonplaceholder.typicode.com/todos")
-    loop_through_list(todos)
+    # loop_through_list(todos)
+    loop_through_index_numbers(todos)
 
 
 if __name__ == '__main__':
