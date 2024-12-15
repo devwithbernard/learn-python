f14babfd4fd33599a338ab31ba401158c74c965a
Bernard Konan <devwithbernard@gmail.com>
2024-12-15 22:40:40 +0000

    Loop though a list with for .. in

---
+++ 5-lists/loop-lists/main.py
@@ -0,0 +1,38 @@
+"""
+Loop through list
+"""
+from typing import List
+import requests
+
+
+def get_todos(url: str) -> List[dict]:
+    try:
+        response: requests.Response = requests.request('GET', url)
+        if response.status_code == 200:
+            todos: List[dict] = response.json()
+            return todos
+    except requests.HTTPError as e:
+        print(e.__str__())
+
+
+def loop_through_list(sequence: List[dict]) -> None:
+    for item in sequence:
+        if item["id"] == 5:
+            break
+
+        print(f"""
+            UserId: {item["userId"]}
+                id: {item["id"]}
+             title: {item["title"]}
+             completed: {item["completed"]}
+        """)
+        print("-"*100)
+
+
+def main() -> None:
+    todos: List[dict] = get_todos("https://jsonplaceholder.typicode.com/todos")
+    loop_through_list(todos)
+
+
+if __name__ == '__main__':
+    main()
