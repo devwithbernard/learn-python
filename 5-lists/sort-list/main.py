96e219ee39508f3cacae0a0b91d119b8243166e5
Bernard Dev <devwithbernard@gmail.com>
2024-12-28 09:09:50 +0000

    Sorting objects by an attribute

--- 5-lists/sort-list/main.py
+++ 5-lists/sort-list/main.py
@@ -5,6 +5,15 @@ List objects have a sort() method that will sort the list alphanumerically, asce
 from typing import List, Any
 
 
+class Person(object):
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+    def __repr__(self) -> str:
+        return f'<Person({self.name}, {self.age})>'
+
+
 def main() -> None:
     fruits: List[str] = ["Orange", "Mango", "Kiwi", "Pineapple", "Banana"]
 
@@ -37,6 +46,18 @@ def main() -> None:
     print(custom_sort([200, 500, 65, 42, 230], key=lambda number: number - 50))
     print(custom_sort(fruits, key=lambda word: len(word)))
     print(custom_sort([100, -400, -22, 17, 234_000], key=abs))
+
+    def sort_persons_by_age(persons: List[Person]) -> List[Person]:
+        sorted_objects: List[Any] = sorted(persons, key=lambda person: person.age, reverse=False)
+        return sorted_objects
+
+    persons: List[Person] = [
+        Person("Roman", 41),
+        Person("John", 23),
+        Person("Alice", 36),
+    ]
+
+    print(sort_persons_by_age(persons))
 
 
 if __name__ == '__main__':
