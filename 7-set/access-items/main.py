65fde18157c496e80335ef03ae5fd8868f25e82d
Bernard Dev <devwithbernard@gmail.com>
2025-01-08 22:44:58 +0000

    Loop through set items

--- 7-set/access-items/main.py
+++ 7-set/access-items/main.py
@@ -7,10 +7,19 @@ def main() -> None:
     fruits: set[str] = {'Banana', 'Apple', 'Cherry'}
 
     def access_items(sequence: set[str]) -> None:
-        (first_item, second_item,*rest, last_item) = tuple(sequence)
+        (first_item, second_item, *rest, last_item) = tuple(sequence)
         print(first_item, second_item, last_item)
 
     access_items(fruits)
+
+    def loop_through_set(sequence: set[str]) -> None:
+        if len(sequence) == 0:
+            print("Empty sequence")
+        else:
+            for index, item in enumerate(sequence):
+                print(f"Index n°{index} => {item}")
+
+    loop_through_set(fruits)
 
 
 if __name__ == '__main__':
