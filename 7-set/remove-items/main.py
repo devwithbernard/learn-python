04ee2de8f948ab6c9944f41e1f25bffbe235c79e
Bernard Dev <devwithbernard@gmail.com>
2025-01-10 12:08:34 +0000

    Discard item from a setÃ

--- 7-set/remove-items/main.py
+++ 7-set/remove-items/main.py
@@ -16,6 +16,13 @@ def main() -> None:
     new_fruits: set[str] = remove_set_item(fruits, 'Banana')
     print(new_fruits)
 
+    def discard_set_item(sequence: set[str], item: str) -> set[str]:
+        sequence.discard(item)
+        return sequence
+
+    new_fruits_after_discarded_item: set[str] = discard_set_item(fruits, 'Cherry')
+    print(new_fruits_after_discarded_item)
+
 
 if __name__ == '__main__':
     main()
