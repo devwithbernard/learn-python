9b0b9b1737bd9b89ada92a0a5405fac7f5dd0071
Bernard Dev <devwithbernard@gmail.com>
2025-01-15 23:30:19 +0000

    Dictionary items

--- 8-dictionary/main.py
+++ 8-dictionary/main.py
@@ -10,6 +10,14 @@ def main() -> None:
 
     print(create_dictionary('fruit', 'Orange'))
 
+    def dictionary_items(dictionary: dict) -> set:
+        values = set()
+        for key in dictionary:
+            values.add(dictionary[key])
+        return values
+
+    print(dictionary_items({'brand': "Ford", 'model': "Mustang", 'year': 1964}))
+
 
 if __name__ == '__main__':
     main()
