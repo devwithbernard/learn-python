e1eff7b10dfa1aa5482205f5444134e17abdb8cf
Bernard Dev <devwithbernard@gmail.com>
2025-01-15 23:49:34 +0000

    Extract dictionary items

--- 8-dictionary/main.py
+++ 8-dictionary/main.py
@@ -1,7 +1,9 @@
 """
 Introduction to python dictionary datatype
 """
-from typing import Dict
+from typing import Dict, List
+from pprint import pprint
+import requests
 
 
 def main() -> None:
@@ -17,6 +19,26 @@ def main() -> None:
         return values
 
     print(dictionary_items({'brand': "Ford", 'model': "Mustang", 'year': 1964}))
+
+    def get_user(index: int) -> List[tuple]:
+        user = requests.get(f"https://jsonplaceholder.typicode.com/users/{index}").json()
+
+        def extract_key_value(user_dict: dict, parent_key='') -> List[tuple]:
+            key_value: List[tuple] = []
+
+            for key, value in user_dict.items():
+                combined_key = f"{parent_key}.{key}" if parent_key else key
+
+                if isinstance(value, dict):
+                    key_value.extend(extract_key_value(value, combined_key))
+                else:
+                    key_value.append((combined_key, value))
+
+            return key_value
+
+        return extract_key_value(user)
+
+    pprint(get_user(2))
 
 
 if __name__ == '__main__':
