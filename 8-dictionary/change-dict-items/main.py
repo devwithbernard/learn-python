ca0795b4ee0887adaecfb3131504b9b383374bdc
Bernard Dev <devwithbernard@gmail.com>
2025-01-22 23:20:01 +0000

    update a dictionaryÃ

--- 8-dictionary/change-dict-items/main.py
+++ 8-dictionary/change-dict-items/main.py
@@ -21,6 +21,15 @@ def main() -> None:
     new_data: Any = change_dict_items(car, 'brad', 'Bugatti')
     print(f"updated data: ", new_data)
 
+    # Update dictionary
+    def update_dict(origin_data:dict, coming_data: dict) -> dict:
+        copy_origin_data: dict = origin_data.copy()
+        copy_origin_data.update(coming_data)
+        return copy_origin_data
+
+    updated_dict: dict = update_dict(car, {'price': 1_000_000})
+    print(f"Updated data: {updated_dict}")
+
 
 if __name__ == '__main__':
     main()
