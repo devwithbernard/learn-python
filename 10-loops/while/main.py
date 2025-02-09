09fa30ddf1879e66b826409a3ade7770d88b3c9b
Bernard Dev <devwithbernard@gmail.com>
2025-02-09 00:32:14 +0000

    Print value with whiile looop

---
+++ 10-loops/while/main.py
@@ -0,0 +1,15 @@
+"""
+While Loop
+"""
+
+
+def main() -> None:
+    # print value
+    index: int = 0
+    while index < 10:
+        index += 1
+        print(index)
+
+
+if __name__ == '__main__':
+    main()
