685888ab5ee56dc69a3861808a44fa1414457f91
Bernard Dev <devwithbernard@gmail.com>
2025-02-09 00:38:49 +0000

    Break statement

--- 10-loops/while/main.py
+++ 10-loops/while/main.py
@@ -10,6 +10,17 @@ def main() -> None:
         index += 1
         print(index)
 
+    # Break statement
+    number: int = 10
+
+    while number > 0:
+        if number % 2 == 0:
+            print(f"{number} is even.")
+        else:
+            print(f"{number} is odd.")
+
+        number -= 1
+
 
 if __name__ == '__main__':
     main()
