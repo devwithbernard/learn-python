ed1b3c3af1dd472019aaa43c4d56fa0aba531cd4
Konan Bernard <devwithbernard@gmail.com>
2025-02-15 22:11:28 +0000

    Recursion definition

---
+++ 11-functions/recursions.py
@@ -0,0 +1,4 @@
+"""
+Recursion is a fundamental concept in programming where a function calls itself to solve smaller instances of a problem.
+It is particularly useful for problems that can be broken down into subproblems of the same type.
+"""
