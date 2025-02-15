5c9b3d22f808734dec3bfeb18beda8faafc65e24
Konan Bernard <devwithbernard@gmail.com>
2025-02-15 18:03:24 +0000

    Define a block of function

--- 11-functions/main.py
+++ 11-functions/main.py
@@ -5,3 +5,17 @@ You can pass data, known as parameters, into a function.
 
 A function can return data as a result.
 """
+
+
+# Define a block of function
+
+def greet() -> None:
+    print("Hello, World!")
+
+
+def main() -> None:
+    greet()
+
+
+if __name__ == '__main__':
+    main()
