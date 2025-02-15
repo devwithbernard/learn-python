928719f29215aba750da2e450d26f2495e3ce7ea
Konan Bernard <devwithbernard@gmail.com>
2025-02-15 22:13:38 +0000

    Recursion explanation

--- 11-functions/recursions.py
+++ 11-functions/recursions.py
@@ -1,4 +1,9 @@
 """
 Recursion is a fundamental concept in programming where a function calls itself to solve smaller instances of a problem.
 It is particularly useful for problems that can be broken down into subproblems of the same type.
+
+
+A recursive function has two main parts:
+    Base Case –> The condition that stops recursion.
+    Recursive Case –> The function calls itself with a smaller input.
 """
