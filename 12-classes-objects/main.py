f591d1e9a7393e5dd4bede18ec3767a279c05a30
Konan Bernard <devwithbernard@gmail.com>
2025-02-18 18:34:19 +0000

    Definition of Object-Oriented Programming (OOP)

---
+++ 12-classes-objects/main.py
@@ -0,0 +1,4 @@
+"""
+Object-Oriented Programming (OOP) is a paradigm that helps structure programs using classes and objects.
+It promotes code reusability, encapsulation, abstraction, inheritance, and polymorphism.
+"""
