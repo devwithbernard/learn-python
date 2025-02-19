e83f423065391651a4db569f6108321ff25e36fe
Bernard Dev <devwithbernard@gmail.com>
2025-02-19 22:35:08 +0000

    Class property

--- 12-classes-objects/main.py
+++ 12-classes-objects/main.py
@@ -10,6 +10,15 @@ class Car:
     def __init__(self, brand: str, color: str) -> None:
         self.brand: str = brand
         self.color: str = color
+        self.__mean_speed = 80.0
+
+    @property
+    def mean_speed(self) -> float:
+        return self.__mean_speed
+
+    @mean_speed.setter
+    def mean_speed(self, value: float):
+        self.__mean_speed = value
 
     def description(self) -> str:
         return f"{self.brand} car of {self.color} color"
@@ -21,6 +30,15 @@ def main() -> None:
 
     car_2: Car = Car('Peugeot', 'blue')
     print(car_2.description())
+
+    car_2.mean_speed = 80.0
+
+    print(f"""
+    Car 
+       brand: {car_2.brand}
+       color: {car_2.color}
+       mean_speed: {car_2.mean_speed} km/h
+    """)
 
 
 if __name__ == '__main__':
