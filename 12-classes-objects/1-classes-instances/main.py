ea0cfc943f7ff372bcc7b9313e1451e802d91a16
Bernard Dev <devwithbernard@gmail.com>
2025-02-20 21:36:36 +0000

    Class method

--- 12-classes-objects/1-classes-instances/main.py
+++ 12-classes-objects/1-classes-instances/main.py
@@ -10,6 +10,9 @@ class Employee:
         self.email = email
         self.pay = pay
 
+    def fullname(self) -> str:
+        return f"{self.firstname} {self.lastname}"
+
 
 def main() -> None:
     employee_1: Employee = Employee("Bernard", "Konan", "bernard.konan@yahoo.fr", 50_000)
@@ -21,6 +24,9 @@ def main() -> None:
     # Print some infos
     print("Employee 1 email: ", employee_1.email)
     print("Employee 2 email: ", employee_2.email)
+
+    print(f"Full name of employee 1: {employee_1.fullname()}")
+    print(f"Full name of employee 2: {employee_2.fullname()}")
 
 
 if __name__ == '__main__':
