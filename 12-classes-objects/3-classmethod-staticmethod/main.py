9792ee224ae105b9be618d92d2dca64ad4b00ae4
Konan Bernard <devwithbernard@gmail.com>
2025-02-24 08:03:32 +0000

    Math operation class using static method

--- 12-classes-objects/3-classmethod-staticmethod/main.py
+++ 12-classes-objects/3-classmethod-staticmethod/main.py
@@ -20,6 +20,37 @@ class HTTP:
             print(f"Request Error occurs: {http_error}")
 
 
+class MathOperation:
+    PI: float = 3.14
+
+    @staticmethod
+    def circle_area(cls, radius: float) -> float:
+        return cls.PI * (radius ** 2)
+
+    @staticmethod
+    def circle_perimeter(cls, radius: float) -> float:
+        return 2 * cls.PI * radius
+
+    @staticmethod
+    def add(cls, x: float, y: float) -> float:
+        return x + y
+
+    @staticmethod
+    def times(cls, x: float, y: float) -> float:
+        return x * y
+
+    @staticmethod
+    def subtraction(cls, x: float, y: float) -> float:
+        return x - y
+
+    @staticmethod
+    def divide(cls, x: float, y: float) -> float:
+        try:
+            return x / y
+        except ZeroDivisionError as error:
+            print(f"Impossible to divide by 0: {error}")
+
+
 def main() -> None:
     HTTP.url = 'https://jsonplaceholder.typicode.com/posts/1'
     post: dict = HTTP.get(HTTP)
@@ -27,6 +58,27 @@ def main() -> None:
     if post:
         for key, value in post.items():
             print(f"{key} => {value}") if key != 'userId' else None
+
+    # Math calculus
+    radius: float = float(input("Enter circle radius: "))
+    number_1: float = float(input("Enter first number: "))
+    number_2: float = float(input("Enter second number: "))
+
+    # Circle Perimeter and Area
+    print(f"""
+    Circle:
+        Perimeter: {MathOperation.circle_perimeter(MathOperation, radius)}
+        Area: {MathOperation.circle_area(MathOperation, radius)}
+    """)
+
+    # Basic Math Operation
+    print(f"""
+    Math operation:
+      {number_1} + {number_2} = {MathOperation.add(MathOperation, number_1, number_2)}
+      {number_1} - {number_2} = {MathOperation.subtraction(MathOperation, number_1, number_2)}
+      {number_1} x {number_2} = {MathOperation.times(MathOperation, number_1, number_2)}
+      {number_1} / {number_2} = {MathOperation.times(MathOperation, number_1, number_2)}
+    """)
 
 
 if __name__ == '__main__':
