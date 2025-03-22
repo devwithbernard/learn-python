990428bc3defa5ca9389f957970cbb4c7634cfaf
Bernard Dev <devwithbernard@gmail.com>
2025-03-22 12:02:10 +0000

    Search feature

--- 12-classes-objects/4-exercise/features/bookManagement.py
+++ 12-classes-objects/4-exercise/features/bookManagement.py
@@ -65,6 +65,26 @@ class BookLibrary:
 
             cls.categories[category].append(book)
 
+    @classmethod
+    def search(cls, title: str, author: str = None):
+        if not title or not author:
+            print("No book found!")
+
+        found_books: List[dict] = [book for book in cls.books if book['title'].upper() == title.upper()]
+        number_of_books_found: int = len(found_books)
+
+        if number_of_books_found > 0:
+            print(f"{number_of_books_found} found!")
+            for found_book in found_books:
+                print(f"{found_book['author']} => {found_book['title']}")
+        else:
+            print("No book found!")
+
+        if author and title:
+            found_books_by_author: List[dict] = [book for book in cls.books if book['author'].upper() == author.upper()]
+            if found_books_by_author:
+                print(found_books_by_author)
+
 
 if __name__ == "__main__":
     b0: Book = Book('Hey', 'Hi', '2010/09/15', 'Fiction')
