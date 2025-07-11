6e1199ccf434d0e49ea703b346b925d4da7c349c
Bernard Konan <devwithbernard@gmail.com>
2025-07-11 17:17:35 +0000

    More than one input

--- 02-information-from-user/main.py
+++ 02-information-from-user/main.py
@@ -6,6 +6,14 @@ print("Hi there, " + name)
 age: str = input("How old are You? ")
 print("You are " + age + " years old.")
 
+# More than one input
+firstname: str = input("What is your firstname? ")
+lastname: str = input("What is your lastname? ")
+email: str = input("What is your email address? ")
+
+print("Information about you: ")
+print("Your name: " + firstname + " " + lastname)
+print("Your email address: " + email)
 
 # Exercises
 """
@@ -14,3 +22,16 @@ Write a program which asks for the user's name and then prints it twice, on two 
 username: str = input("What is your name? ")
 print(username)
 print(username)
+
+"""
+Write a program which asks for the user's name and address
+"""
+first_name: str = input("What is your first name? ")
+last_name: str = input("What is your last name? ")
+street: str = input("What's is your street's address? ")
+city_postal: str = input("Your City and postal code? ")
+
+print(first_name)
+print(last_name)
+print(street)
+print(city_postal)
