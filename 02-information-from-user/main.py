40c672e8321541f818770ff6f277a6b95be6c80b
Bernard Konan <devwithbernard@gmail.com>
2025-07-11 14:26:58 +0000

    decomment block of code

--- 02-information-from-user/main.py
+++ 02-information-from-user/main.py
@@ -1,10 +1,10 @@
-# # Prompt a question and print a message
-#
-# name: str = input("What is your name? ")
-# print("Hi there, " + name)
-#
-# age: str = input("How old are You? ")
-# print("You are " + age + " years old.")
+# Prompt a question and print a message
+
+name: str = input("What is your name? ")
+print("Hi there, " + name)
+
+age: str = input("How old are You? ")
+print("You are " + age + " years old.")
 
 
 # Exercises
