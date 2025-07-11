9c24eca5019d39c4ac04694d998fb3acbd89e4d0
Bernard Konan <devwithbernard@gmail.com>
2025-07-11 14:20:24 +0000

    Exercise: print user name twice

--- 02-information-from-user/main.py
+++ 02-information-from-user/main.py
@@ -1,7 +1,16 @@
-# Prompt a question and print a message
+# # Prompt a question and print a message
+#
+# name: str = input("What is your name? ")
+# print("Hi there, " + name)
+#
+# age: str = input("How old are You? ")
+# print("You are " + age + " years old.")
 
-name: str = input("What is your name? ")
-print("Hi there, " + name)
 
-age: str = input("How old are You? ")
-print("You are " + age + " years old.")
+# Exercises
+"""
+Write a program which asks for the user's name and then prints it twice, on two consecutive lines.
+"""
+username: str = input("What is your name? ")
+print(username)
+print(username)
