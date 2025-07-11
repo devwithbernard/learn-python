59e77e3234947c8837c4c6310b69e1149176ede9
Bernard Konan <devwithbernard@gmail.com>
2025-07-11 14:12:08 +0000

    Ask user age and name. Then display these infos

---
+++ 02-information-from-user/main.py
@@ -0,0 +1,7 @@
+# Prompt a question and print a message
+
+name: str = input("What is your name? ")
+print("Hi there, " + name)
+
+age: str = input("How old are You? ")
+print("You are " + age + " years old.")
