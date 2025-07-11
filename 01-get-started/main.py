79e7b9be7f0bc2b484eab84a50f0251c7c96c07e
Bernard Konan <devwithbernard@gmail.com>
2025-07-11 11:42:49 +0000

    A program of multiple commands and exercise

--- 01-get-started/main.py
+++ 01-get-started/main.py
@@ -2,9 +2,33 @@
 
 print("Hi there!")
 
+# A program of multiple commands
+print("Welcome to Introduction of Programming!")
+print("First, we will practice using the print command.")
+print("This program prints three lines of text on the screen.")
 
 # Exercises
 """
 Write a program that print an emoticon
 """
 print("Hello mate 😃!")
+
+
+"""
+This program is supposed to print the followings names in alphabetical order:
+- Simeoni
+- Juhani
+- Eero
+- Lauri
+- Aapo
+- Tuomas
+- Timo
+"""
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
