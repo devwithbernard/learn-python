c873d852645c1517317c3ab952cf0062116e680a
Bernard Konan <devwithbernard@gmail.com>
2025-07-12 23:18:44 +0000

    Conditionals: age of maturity exerciseÃ

--- 01-more-conditionals/main.py
+++ 01-more-conditionals/main.py
@@ -16,3 +16,17 @@ if new_number < 0:
     print(f"{new_number} is negative")
 else:
     print(f"{new_number} is positive or zero")
+
+# Exercises
+"""
+Age of maturity
+"""
+
+age = int(input("How old are you? "))
+
+if age < 0:
+    print("You are not born!")
+elif age < 18:
+    print("You are not of age!")
+else:
+    print("You are of age!")
