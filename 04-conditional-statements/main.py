ce5c9735eb20e9bc2e5967535f09d34c552d4a8e
Bernard Konan <devwithbernard@gmail.com>
2025-07-12 16:24:19 +0000

    Conditionals statements: Exercises

--- 04-conditional-statements/main.py
+++ 04-conditional-statements/main.py
@@ -37,3 +37,63 @@ elif mean <= 20:
     print("You pass to next class.")
 else:
     print("⛔ Mean must be lower or equal to 20.")
+
+"""
+Write a program which asks the user for an integer number.
+The program should print out "Orwell" if the number is exactly 1984, and otherwise dot nothing.
+"""
+
+entry_number = int(input("Please type the number: "))
+
+if entry_number == 1984:
+    print("Orwell")
+
+"""
+The Absolute Value
+"""
+print("---- THE ABSOLUTE VALUE ----")
+user_entry = int(input("Please type in a number: "))
+
+if user_entry < 0:
+    print(f"Absolute value: {-1 * user_entry}")
+else:
+    print(f"Absolute value: {user_entry}")
+
+"""
+Soup or no soup
+"""
+
+username = input("Please tell me your name: ")
+portion_price = 5.90
+
+if username.lower() != "jerry":
+    portion_quantity = int(input("How many portions of soup? "))
+    total = portion_price * portion_quantity
+
+    print(f"The total cost is {total:,.2f}")
+
+print("Next please!")
+
+number = int(input("Please a number: "))
+quotient = number // 10
+message = ""
+
+if number >= 0:
+    if quotient >= 10:
+        message = "This number is smaller than 1000"
+    elif quotient >= 1:
+        message = """
+This number is smaller than 1000
+This number is smaller than 100"""
+    elif quotient == 0:
+        message = """
+This number is smaller than 1000
+This number is smaller than 100
+This number is smaller than 10"""
+    print(message)
+    print("Thank you!")
+else:
+    print("Negative number not allowed!")
+
+
+
