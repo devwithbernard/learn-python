b59869c5409d13055dc804e0868c5421338f6b59
Bernard Konan <devwithbernard@gmail.com>
2025-07-12 23:14:32 +0000

    Conditionals: Write the previous program with else statement

--- 01-more-conditionals/main.py
+++ 01-more-conditionals/main.py
@@ -7,3 +7,12 @@ if number < 0:
 
 if number >= 0:
     print("The number is positive or zero")
+
+# Write previous program with else statement
+
+new_number = int(input("Please type in a new number: "))
+
+if new_number < 0:
+    print(f"{new_number} is negative")
+else:
+    print(f"{new_number} is positive or zero")
