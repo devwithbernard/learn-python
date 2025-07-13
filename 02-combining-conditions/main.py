a87ef3631402488b16bbf254b828507ee425ab66
Bernard Konan <devwithbernard@gmail.com>
2025-07-13 15:29:31 +0000

    Combining conditions: compare numbers

---
+++ 02-combining-conditions/main.py
@@ -0,0 +1,9 @@
+# Logical operators
+
+number = int(input("Please type in a number: "))
+
+if (number >= 5) and (number <= 8):
+    print(f"The number is between 5 and 8.")
+
+if (number < 5) or (number > 8):
+    print("The number is not within the range of 5 to 8.")
