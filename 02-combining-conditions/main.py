2fb86c5c2ebe8ab6124fe219676dfe4b705b6d7b
Bernard Konan <devwithbernard@gmail.com>
2025-07-13 16:11:17 +0000

    Combining conditions: Age check exercise

--- 02-combining-conditions/main.py
+++ 02-combining-conditions/main.py
@@ -26,3 +26,17 @@ else:
     greatest = n4
 
 print(f" {greatest} is the greatest of the numbers")
+
+# Exercises
+"""
+Age check
+"""
+
+age = int(input("What is your age? "))
+
+if age <= 0:
+    print("That must be a mistake.")
+elif age < 10:
+    print("I suspect you can't write quite yet...")
+else:
+    print(f"Ok, you're {age} years old")
