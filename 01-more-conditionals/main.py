e93edf3f50fdfd80909a2808bc7bbaee94786e1b
Bernard Konan <devwithbernard@gmail.com>
2025-07-13 14:47:43 +0000

    Conditionals: Greater than or equal to exercise

--- 01-more-conditionals/main.py
+++ 01-more-conditionals/main.py
@@ -41,3 +41,17 @@ elif age < 18:
     print("You are not of age!")
 else:
     print("You are of age!")
+
+"""
+Greater than or equal to
+"""
+
+first_number = int(input("Please type in the first number: "))
+second_number = int(input("Please type in another number: "))
+
+if first_number > second_number:
+    print(f"The greater number was: {first_number}")
+elif second_number > first_number:
+    print(f"The greater number was: {second_number}")
+else:
+    print(f"The numbers are equal!")
