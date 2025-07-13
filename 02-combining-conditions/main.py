a9c7aa48ee6c96bfc051fe3db3fc881014915c83
Bernard Konan <devwithbernard@gmail.com>
2025-07-13 16:22:58 +0000

    Combining conditions: grades and points exercise

--- 02-combining-conditions/main.py
+++ 02-combining-conditions/main.py
@@ -40,3 +40,26 @@ elif age < 10:
     print("I suspect you can't write quite yet...")
 else:
     print(f"Ok, you're {age} years old")
+
+"""
+Grades and points
+"""
+points = int(input("How many points [0-100]: "))
+grade = None
+
+if points < 0 or points > 100:
+    grade = "impossible!"
+elif 0 <= points <= 49:
+    grade = "fail"
+elif 50 <= points <= 59:
+    grade = 1
+elif 60 <= points <= 69:
+    grade = 2
+elif 70 <= points <= 79:
+    grade = 3
+elif 80 <= points <= 89:
+    grade = 4
+elif 90 <= points <= 100:
+    grade = 5
+
+print(f"Grade: {grade}")
