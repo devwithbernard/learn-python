e4216b80b049dfca2f108f0ad880a79a4656f74a
Bernard Konan <devwithbernard@gmail.com>
2025-07-14 14:09:57 +0000

    While loop: print the square of a given number

---
+++ 03-simple-loops/main.py
@@ -0,0 +1,11 @@
+# While Loops
+
+while True:
+    number = int(input("Please, type in a number, -1 to quit: "))
+
+    if number == -1:
+        break
+
+    print(f"n² = {number**2}")
+
+print(f"Thanks and bye!")
