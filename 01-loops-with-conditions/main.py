74ddf2afb97aa440cf69ae0d9fb8d9683465fb36
Bernard Konan <devwithbernard@gmail.com>
2025-07-17 08:31:09 +0000

    Loops with conditions: Initialisation, condition and update

---
+++ 01-loops-with-conditions/main.py
@@ -0,0 +1,10 @@
+# TODO: Initialisation, condition and update
+
+number = int(input("Please type in a number: "))  # Initialisation
+
+while number < 10:  # Condition
+    print(number)
+    number += 1
+
+print("Execution finished.")
+
