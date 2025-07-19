37b754cd559498342494ca8d50c9d4cb6f3d1ca2
Bernard Konan <devwithbernard@gmail.com>
2025-07-19 12:22:07 +0000

    Working with strings: Exercise -> Find the longest word

--- 02-working-with-strings/main.py
+++ 02-working-with-strings/main.py
@@ -57,3 +57,23 @@ string = input("Please type in a string: ")
 amount = int(input("Please type i an amount: "))
 
 print(string * amount)
+
+# TODO: The longest string
+strings = ""
+stop = False
+
+while not stop:
+    string = input("Type in a string ('Q' or 'Quit' to exit): ")
+
+    if string.lower() in ('q', 'quit'):
+        stop = True
+
+    if not stop:
+        strings += f"{string}, "
+
+if len(strings):
+    words = strings.strip(", ").split(", ")
+    longest_word = max(words, key=len)
+    print("The longest word of strings: ")
+    print(f"word: {longest_word}\nlength: {len(longest_word)}")
+
