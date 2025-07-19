932b1f36971d1794e0a6eb048e5d5df5958af288
Bernard Konan <devwithbernard@gmail.com>
2025-07-19 11:41:55 +0000

    Working with strings: String Repetition

--- 02-working-with-strings/main.py
+++ 02-working-with-strings/main.py
@@ -9,3 +9,34 @@ end = "ple"
 word = begin + end
 
 print("Final word: " + word)
+
+# TODO: String Repetition
+fruit = "Banana"
+
+print(f"Repeat '{fruit}' 3 times: {fruit * 3}")
+
+# Build pyramid with string repetition
+base = 10
+row = "*"
+
+while base > 0:
+    print(" " * base + row)
+    row += "**"
+    base -= 1
+
+# Build rectangle with string repetition
+length = 5
+width = 2 * length + 10
+row = " * "
+while length > 0:
+    print(row * width)
+    length -= 1
+
+# Build isosceles triangle
+base = 5
+row = 0
+while row < 5:
+    space = " " * (base - row + 1)
+    stars = "*" * (2 * row + 1)
+    print(space + stars)
+    row += 1
