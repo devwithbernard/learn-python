30e6886b551b568cf14e36512784fc6df91dddfc
Bernard Konan <devwithbernard@gmail.com>
2025-07-20 19:45:41 +0000

    More loops: Exercise -> Table of multiplication

--- part-3/03-more-loops/main.py
+++ part-3/03-more-loops/main.py
@@ -63,3 +63,16 @@ while number > 0:
         i += 1
     print()
     number -= 1
+
+# Exercises
+
+# TODO: multiplication
+number = int(input("Please type in a number: "))
+i = 1
+while i <= number:
+    j = 1
+    while j <= number:
+        print(f"{i} x {j} = {i * j}")
+        j += 1
+    i += 1
+
