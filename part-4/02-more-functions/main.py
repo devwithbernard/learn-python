a48bb93e6cea3444c820ad819f44b04eec78e887
Bernard Konan <devwithbernard@gmail.com>
2025-07-26 07:49:13 +0000

    More functions: Illustrate parameters and arguments differenceÃ

---
+++ part-4/02-more-functions/main.py
@@ -0,0 +1,16 @@
+"""
+More about functions
+"""
+
+
+# TODO: The parameters and arguments of a function
+def greet(name: str) -> None:
+    print("Hello", name)
+
+
+def sum_numbers(a: float, b: float) -> None:
+    print(f"{a} + {b} = {a + b}")
+
+
+greet("Konan bernard")
+sum_numbers(1.55, 3.66)
