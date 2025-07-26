9bcd9d0de556ab4343fe38d7b1081470c9794fba
Bernard Konan <devwithbernard@gmail.com>
2025-07-26 08:24:01 +0000

    More functions: function calls with a function

--- part-4/02-more-functions/main.py
+++ part-4/02-more-functions/main.py
@@ -15,6 +15,20 @@ def sum_numbers(a: float, b: float) -> None:
 greet("Konan bernard")
 sum_numbers(1.55, 3.66)
 
+
+# TODO: Function cals within function calls
+
+def greet_many_times(name: str, times: int) -> None:
+    if times == 0 or not name:
+        print("nobody has been greet yet!")
+    else:
+        while times > 0:
+            greet(name)
+            times -= 1
+
+
+greet_many_times("Konan Bernard", 3)
+
 """
 Exercises
 """
