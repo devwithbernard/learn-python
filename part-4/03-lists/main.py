bd559cb39ce47b52a26386ea53d81d159860892e
Bernard Konan <devwithbernard@gmail.com>
2025-07-28 07:53:13 +0000

    Lists: Create some lists

---
+++ part-4/03-lists/main.py
@@ -0,0 +1,14 @@
+"""
+Lists in Python
+"""
+
+# TODO: Create a list
+
+empty_list: list[None] = []
+print(empty_list, type(empty_list))
+
+numbers: list[list[int]] = [[1, 3, 5], [2, 4, 6]]
+print(numbers)
+
+fruits: list[str] = ['Banana🍌', 'Mango🥭', 'Apple🍏', 'Cherry🍓']
+print(fruits)
