11dc57be80916a78bb2a8ae1b43b4b4398886956
Bernard Konan <devwithbernard@gmail.com>
2025-07-29 07:01:42 +0000

    Lists: Accessing items to a list

--- part-4/03-lists/main.py
+++ part-4/03-lists/main.py
@@ -12,3 +12,19 @@ print(numbers)
 
 fruits: list[str] = ['Banana🍌', 'Mango🥭', 'Apple🍏', 'Cherry🍓']
 print(fruits)
+
+# TODO: Accessing items in a list
+banana: str = fruits[0]
+mango: str = fruits[1]
+cherry: str = fruits[len(fruits) - 1]
+
+print(f"Banana => {banana[-1]}")
+print(f"Mango => {mango[-1]}")
+print(f"Cherry => {cherry[-1]}")
+
+print("-" * 15)
+
+# Print all values in a list
+for fruit in fruits:
+    print(f"{fruit[:-1]} => {fruit[-1]}")
+
