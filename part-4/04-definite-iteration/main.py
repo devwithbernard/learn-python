728bd22ad0b1ed3544cc920a495dd5aae8a4503e
Bernard Konan <devwithbernard@gmail.com>
2025-07-30 20:21:14 +0000

    Loop: Loop through list using while loop

---
+++ part-4/04-definite-iteration/main.py
@@ -0,0 +1,12 @@
+"""
+Iterate Sequence using loop
+"""
+
+# TODO: Iterate using while loop
+
+numbers: list[int] = [3, 2, 1, 4, 5]
+index: int = 0
+
+while index < len(numbers):
+    print(numbers[index])
+    index += 1
