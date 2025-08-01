8a1db181e2556fe63b3814a60149b7a7378ccf74
Bernard Konan <devwithbernard@gmail.com>
2025-08-01 22:51:42 +0000

    More strings and lists: Exercise -> Reversed everything

--- part-4/05-more-strings-and-lists/main.py
+++ part-4/05-more-strings-and-lists/main.py
@@ -23,3 +23,41 @@ print(my_list[6:2:-1])
 string: str = input("Please type in a string: ")
 reversed_string: str = string[::-1]
 print(reversed_string)
+
+"""
+Exercises
+"""
+
+
+# TODO: Everything reversed
+
+def everything_reversed(strings: list[str]) -> list[str]:
+    """
+    Returns a new list with each string reversed and the list order reversed.
+
+    This function takes a list of strings, reverses the characters in each string,
+    and also reverses the order of the strings in the list.
+
+    Args:
+        strings (list[str]): A list of strings.
+
+    Returns:
+        list[str]: A new list containing the reversed strings in reversed order.
+
+    Example:
+        >>> everything_reversed(["hello", "world", "python"])
+        ['nohtyp', 'dlrow', 'olleh']
+    """
+    new_strings: list[str] = []
+
+    for string in strings:
+        reversed_string = string[::-1]
+        new_strings.append(reversed_string)
+
+    return new_strings[::-1]
+
+
+new_my_lists: list[str] = ["Hi", "there", "example", "one more"]
+reversed_lists: list[str] = everything_reversed(new_my_lists)
+print(reversed_lists)
+
