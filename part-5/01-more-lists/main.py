617563be0f9b7bbb759cb2e0dd2acb6a964a2261
Bernard Konan <devwithbernard@gmail.com>
2025-08-06 15:21:23 +0000

    More Lists: Matrix

--- part-5/01-more-lists/main.py
+++ part-5/01-more-lists/main.py
@@ -2,6 +2,7 @@
 Learn more list features
 """
 
+
 # TODO: Reversed list
 
 def reversed_list(names: list[str]) -> list[str] | None:
@@ -77,9 +78,88 @@ for person in persons:
 
     print(f"{name}: age {age} years old, height {height} meters.")
 
+# TODO: Matrices
+matrix: list[list[float]] = [[1, 2, 3], [3, 2, 1], [4, 5, 6]]
+
+for row in matrix:
+    for element in row:
+        print(element, end=" ")
+    print()
+
+
+def sum_of_row(matrix: list[list[int]], row_no: int) -> int:
+    """
+    Calculate the sum of all elements in a specific row of a matrix.
+
+    Args:
+        matrix (list[list[int]]): A 2D list representing a matrix of integers.
+        row_no (int): The index of the row to sum (0-based indexing).
+
+    Returns:
+        int: The sum of all integers in the specified row.
+
+    Raises:
+        IndexError: If row_no is greater than or equal to the number of rows
+                   in the matrix.
+
+    Examples:
+        >>> matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        >>> sum_of_row(matrix, 0)
+        6
+        >>> sum_of_row(matrix, 1)
+        15
+        >>> sum_of_row(matrix, 2)
+        24
+        >>> sum_of_row(matrix, 3)
+        IndexError: '3' out of index
+    """
+    if row_no >= len(matrix):
+        raise IndexError(f"'{row_no}' out of index")
+
+    row = matrix[row_no]
+    sum_row = 0
+
+    for item in row:
+        sum_row += item
+
+    return sum_row
+
+
+my_sum = sum_of_row(matrix, 1)
+print(my_sum)
+
+
+def sum_of_col(matrix: list[list[int]], col_no: int) -> int:
+    sum_col = 0
+
+    for row in matrix:
+        sum_col += row[col_no]
+
+    return sum_col
+
+
+m = [[4, 2, 3, 2], [9, 1, 12, 11], [7, 8, 9, 5], [2, 9, 15, 1]]
+
+my_sum = sum_of_col(m, 2)
+print(my_sum)
+
+
+def change_value(matrix: list[list[float]], row_no: int,
+                 col_no: int, new_value: int) -> None:
+    row = matrix[row_no]
+    row[col_no] = new_value
+
+
+m = [[4, 2, 3, 2], [9, 1, 12, 11], [7, 8, 9, 5], [2, 9, 15, 1]]
+
+print(m)
+change_value(m, 2, 3, 1000)
+print(m)
+
 """
 Exercises
 """
+
 
 # TODO: The longest string
 
