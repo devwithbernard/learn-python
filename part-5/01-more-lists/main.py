936e42714b3052b386f20e8e12e55bae1b65d3aa
Bernard Konan <devwithbernard@gmail.com>
2025-08-06 14:13:23 +0000

    More Lists: the longest string in a list of strings

--- part-5/01-more-lists/main.py
+++ part-5/01-more-lists/main.py
@@ -56,3 +56,48 @@ def unique_numbers(numbers: list[int]) -> list[int]:
 
 numbers = [1, 2, 2, 3, 9, 9, 8, 10]
 print(unique_numbers(numbers))
+
+"""
+Exercises
+"""
+
+# TODO: The longest string
+
+
+def longest_string(strings: list[str]) -> str:
+    """
+     Find and return the longest string from a list of strings.
+
+     Args:
+         strings (list[str]): A list of strings to search through.
+
+     Returns:
+         str: The longest string in the list. If multiple strings have the same
+              maximum length, returns the first one encountered. Returns an empty
+              string if the input list is empty.
+
+     Examples:
+         >>> longest_string(["hello", "world", "python"])
+         'python'
+         >>> longest_string(["a", "bb", "ccc"])
+         'ccc'
+         >>> longest_string([])
+         ''
+         >>> longest_string(["same", "size"])
+         'same'
+     """
+
+    if len(strings) == 0:
+        return ""
+
+    long_str = strings[0]
+
+    for i in range(1, len(strings)):
+        if len(strings[i]) > len(long_str):
+            long_str = strings[i]
+
+    return long_str
+
+
+my_strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+print(longest_string(my_strings))
