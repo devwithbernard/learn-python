769ed1372e45c1575a70dbb28625706210d8b084
Bernard Konan <devwithbernard@gmail.com>
2025-08-06 18:11:26 +0000

    Dictionaries: Set a key:value pair

--- part-5/02-dictionaries/main.py
+++ part-5/02-dictionaries/main.py
@@ -4,10 +4,22 @@ Learn python dictionary
 
 # Dictionary
 
-student: dict = {'name': 'John', 'age': 23, 'studies': ['Computer Science', 'Math', 'Physic', 'Politics']}
+student: dict[str, str | int | list[str]] = {
+    'name': 'John',
+    'age': 23,
+    'studies': ['Computer Science', 'Math', 'Physic', 'Politics']
+}
 
 print("Name: ", student['name'])
 print("Age:", student['age'], 'years old')
 print("Studies:")
 for index, study in enumerate(student['studies']):
     print(f"  {index}. {study}")
+
+
+# Set key: value
+student['phone'] = "+1 899 200 315"
+student['studies'].append("Sociology")
+student['studies'].append("Biology")
+
+print(student)
