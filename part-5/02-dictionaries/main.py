63a60c9fe2dad8f646f1a3a25a6e374253e3da03
Bernard Konan <devwithbernard@gmail.com>
2025-08-07 08:16:14 +0000

    Dictionaries: Traversing a dictionary

--- part-5/02-dictionaries/main.py
+++ part-5/02-dictionaries/main.py
@@ -16,10 +16,30 @@ print("Studies:")
 for index, study in enumerate(student['studies']):
     print(f"  {index}. {study}")
 
-
 # Set key: value
 student['phone'] = "+1 899 200 315"
 student['studies'].append("Sociology")
 student['studies'].append("Biology")
 
 print(student)
+
+# Traversing a dictionary
+employee: dict[str, ...] = {
+    'name': "Jane Smith",
+    'age': 52,
+    'employee_code': "1553B",
+    'email': "janesmith@gmail.com",
+    'role': "Manager",
+    'salary': 1_200_000,
+}
+
+for key in employee:
+    print("key:", key)
+    print("value:", employee[key])
+
+print()
+
+# key:value Pair
+
+for key, value in employee.items():
+    print(f"{key}: {value}")
