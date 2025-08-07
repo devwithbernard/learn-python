21a8be64c842036f6c53656274592f1a9c58f5e5
Bernard Konan <devwithbernard@gmail.com>
2025-08-07 15:15:55 +0000

    Tuple: Exercise -> Find the oldest person

--- part-5/03-tuples/exercises.py
+++ part-5/03-tuples/exercises.py
@@ -1,6 +1,7 @@
 """
 Exercises using Tuples
 """
+import datetime
 
 
 # TODO: Create a tuple
@@ -14,3 +15,28 @@ def create_tuple(x: int, y: int, z: int) -> tuple[int, ...]:
     return return_tuple
 
 print(create_tuple(5, 3, -1))
+
+
+# TODO: The oldest person
+
+def oldest_person(people: list[tuple[str, int]]) -> dict[str, int]:
+    min_birth_year = people[0][1]
+    search_person: dict[str, int] = None
+
+    for person in people:
+        if person[1] < min_birth_year:
+            min_birth_year = person[1]
+            search_person = person
+
+    return search_person
+
+
+p1 = ("Adam", 1977)
+p2 = ("Ellen", 1985)
+p3 = ("Mary", 1953)
+p4 = ("Ernest", 1997)
+people = [p1, p2, p3, p4]
+
+older = oldest_person(people)
+
+print(f"The oldest person is {older[0]}.")
