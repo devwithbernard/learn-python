1c14f32427c481cd14b5038e2564aab5331a51cb
Bernard Konan <devwithbernard@gmail.com>
2025-08-08 16:26:37 +0000

    Reading files: Exercise -> Read a fruits.csv file and print all fruits

--- part-6/01-reading-files/exercises.py
+++ part-6/01-reading-files/exercises.py
@@ -17,3 +17,25 @@ def largest_numbers(file: str) -> int:
 
 
 print("The largest number in the file: ", largest_numbers("files/numbers.txt"))
+
+
+# TODO: Fruits market
+
+def read_fruits(file):
+    contents = []
+
+    with open(file) as f:
+        titles = f.readline().replace("\n", "").replace("ï»¿","").strip().split(";")
+
+        for line in f:
+            rows = line.replace("\n", "").strip().split(";")
+            new_dict = {}
+
+            for i in range(len(titles)):
+                new_dict[titles[i]] = rows[i]
+
+            contents.append(new_dict)
+
+    return contents
+
+print(read_fruits("files/fruits.csv"))
