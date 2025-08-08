d4bf746ade20f9f677fabf6ea165cd666d83630c
Bernard Konan <devwithbernard@gmail.com>
2025-08-08 12:31:33 +0000

    Reading files: Read a csv file

--- part-6/01-reading-files/main.py
+++ part-6/01-reading-files/main.py
@@ -15,4 +15,72 @@ with open("files/example.txt") as f:
 
     for line in f:
         count_line += 1
-        print(f"Line {count_line}:", line.replace("\n",""))
+        print(f"Line {count_line}:", line.replace("\n", ""))
+
+# Reading CSV files
+def format(titles: tuple[str, ...], parts: list[str]) -> dict[str, str]:
+    new_dict = {}
+
+    for i in range(len(titles)):
+        if titles[i] in ('id', 'work_year', 'remote_ratio'):
+            new_dict[titles[i]] = int(parts[i])
+        elif titles[i] in ('salary', 'salary_in_usd'):
+            new_dict[titles[i]] = float(parts[i])
+        else:
+            new_dict[titles[i]] = parts[i]
+
+    return new_dict
+
+
+def get_salaries(employees: dict) -> list[float]:
+    salaries: list[float] = [employee['salary'] for employee in employees]
+
+    return salaries
+
+
+def mean_salaries(salaries: list[float]) -> int:
+    return round(sum(salaries) // len(salaries))
+
+
+titles: tuple[str, ...] = tuple()
+employees: list[dict] = []
+
+
+with open("files/ds_salaries.csv") as file:
+    first_line = file.readline()
+    titles = tuple(first_line.replace("\n","").split(","))
+
+    for line in file:
+        line = line.replace("\n", "")
+        parts: list[str] = line.split(",")
+        employees.append(format(titles, parts))
+
+
+def get_employees_per_job(employees: list[dict]) -> dict[str, list[dict]]:
+    category: dict = {}
+
+    for employee in employees:
+        job_title = employee['job_title'].lower()
+        if job_title not in category:
+            category[job_title] = []
+            category[job_title].append(employee)
+        else:
+            category[job_title].append(employee)
+
+    return category
+
+# Get mean salary
+mean_salary = mean_salaries(get_salaries(employees))
+
+# Employees that gain more than mean salary
+
+paid_employees = [employee for employee in employees if employee['salary'] >= mean_salary]
+
+for paid_employee in paid_employees:
+    print(f"{paid_employee['id']}. {paid_employee['experience_level']} => {paid_employee['salary']}")
+
+# Employee per job
+employees_per_job = get_employees_per_job(employees)
+
+for emp, values in employees_per_job.items():
+    print(f"{emp.title()} -> {len(values)} employee{'s' if len(values) > 1 else ''}")
