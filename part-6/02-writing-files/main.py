317327181704d3529a1cc6343a2efe5db27ed3bd
Bernard Konan <devwithbernard@gmail.com>
2025-08-14 19:59:07 +0000

    Writing files: Appending data to an existing file

--- part-6/02-writing-files/main.py
+++ part-6/02-writing-files/main.py
@@ -16,12 +16,26 @@ def multi_lines(texts: list[str], filename: str) -> None:
     print("File wrote successfully")
 
 
+def append_name(filename: str) -> None:
+    new_name = input("Your name: ")
+    if new_name:
+        with open(filename, mode='a') as file:
+            line = f"Name: {new_name}\n"
+            file.write(line)
+    else:
+        print(f"'{new_name}' is not valid.")
+
+
 def main() -> None:
     # Create a new file
     create_new_file("files/new_file.txt")
 
     # Write multine lines
     multi_lines(['John', 'Emma', 'Jane', 'Luigi'], "files/multi_lines_file.txt")
+
+    # Append name to existing file
+    append_name("files/multi_lines_file.txt")
+
 
 
 if __name__ == '__main__':
