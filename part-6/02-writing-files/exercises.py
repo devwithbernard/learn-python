3649a332556c7616a34a606588277bb843ac93c4
Bernard Konan <devwithbernard@gmail.com>
2025-08-14 22:43:49 +0000

    Writing files: Exercise -> Dairy file

--- part-6/02-writing-files/exercises.py
+++ part-6/02-writing-files/exercises.py
@@ -15,9 +15,46 @@ def inscription() -> None:
             print(f"'{text}'\nadded successfully.")
 
 
+# TODO: Diary
+
+def option() -> int:
+    print("1- Add an entry, 2- Read entries, 0- Quit")
+    choice = input("Function: ")
+    return int(choice)
+
+def save_entry(entry: str, filename: str) -> None:
+    with open(filename, mode='a') as file:
+        line = f"{entry}\n"
+        file.write(line)
+        print("Dairy saved!")
+
+
+def read_entries(filename: str) -> None:
+    print("Entries: ")
+    with open(filename) as file:
+        for line in file:
+            line = line.replace("\n", "")
+            print(line)
+
+
+def entry_point(filename: str) -> None:
+    while True:
+        user_option = option()
+
+        if user_option == 0:
+            print("Bye!")
+            break
+        elif user_option == 1:
+            entry = input("Dairy entry: ")
+            save_entry(entry, filename)
+        elif user_option == 2:
+            read_entries(filename)
+
+
+
 def main() -> None:
     inscription()
-
+    entry_point("exercises-files/dairy.txt")
 
 if __name__ == '__main__':
     main()
