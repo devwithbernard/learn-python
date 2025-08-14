e1232289f768c774e744d3a27abc5347a13e2649
Bernard Konan <devwithbernard@gmail.com>
2025-08-14 17:20:18 +0000

    Writing files: Write multi lines

--- part-6/02-writing-files/main.py
+++ part-6/02-writing-files/main.py
@@ -7,9 +7,21 @@ def create_new_file(filename: str) -> None:
         file.write("I love to code in python")
 
 
+def multi_lines(texts: list[str], filename: str) -> None:
+    if texts:
+        with open(filename, mode='w') as file:
+            for text in texts:
+                line = f"Name: {text}\n"
+                file.write(line)
+    print("File wrote successfully")
+
+
 def main() -> None:
     # Create a new file
     create_new_file("files/new_file.txt")
+
+    # Write multine lines
+    multi_lines(['John', 'Emma', 'Jane', 'Luigi'], "files/multi_lines_file.txt")
 
 
 if __name__ == '__main__':
