1ae3cc9e9321e244ce420e2826b084a621661dca
Bernard Konan <devwithbernard@gmail.com>
2025-08-17 13:09:42 +0000

    Handling errors: Manage an exception

--- part-6/03-handling-errors/main.py
+++ part-6/03-handling-errors/main.py
@@ -9,3 +9,14 @@ if 0 <= age <= 150:
     print("That is a fine age")
 else:
     print("This is not a valid age")
+
+# Exceptions
+try:
+    my_age: int = int(input("Please type in your age: "))
+except ValueError:
+    my_age = -1
+
+if 0 <= my_age <= 150:
+    print(f"{my_age} is a fine age")
+else:
+    print(f"{my_age} is not a valid age")
