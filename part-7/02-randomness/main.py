369fba91d2b6dc61d3fe17c08873d7a64cd5c244
Bernard Konan <devwithbernard@gmail.com>
2025-08-17 16:21:29 +0000

    Randomness: Study loterry number case

--- part-7/02-randomness/main.py
+++ part-7/02-randomness/main.py
@@ -13,3 +13,9 @@ print("The result of the throw:", random.randint(1, 6))
 fruits: list[str] = ["Apple", "Banana", "Mango", "Orange", "Pineapple"]
 random.shuffle(fruits)
 print(fruits)
+
+# Randomness Case Study: Lottery numbers
+number_pool: list[int] = list(range(1, 41))
+random.shuffle(number_pool)
+weekly_draw: list[int] = number_pool[:7]
+print(weekly_draw)
