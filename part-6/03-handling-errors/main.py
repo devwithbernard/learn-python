9f1d3a0850d84c233f8fe75333a9110695412ac6
Bernard Konan <devwithbernard@gmail.com>
2025-08-17 13:15:44 +0000

    Handling errors: Until the number is small

--- part-6/03-handling-errors/main.py
+++ part-6/03-handling-errors/main.py
@@ -20,3 +20,21 @@ if 0 <= my_age <= 150:
     print(f"{my_age} is a fine age")
 else:
     print(f"{my_age} is not a valid age")
+
+# Until the humber is small
+def read_small_number():
+    while True:
+        try:
+            input_str = input("Please type in a small integer: ")
+            number = int(input_str)
+
+            if number <= 100:
+                return number
+        except ValueError as e:
+            print(f"An error occurs: {e}")
+
+        print("This input is invalid!")
+
+
+number = read_small_number()
+print(number, "to the power of three is", number ** 3)
