06cf0f893e1de08cde491cd99599d10bbd638099
Bernard Konan <devwithbernard@gmail.com>
2025-08-18 19:05:02 +0000

    Objects and methods: Define the constructor method

--- part-8/02-defining-classes/main.py
+++ part-8/02-defining-classes/main.py
@@ -5,9 +5,11 @@ Defining classes
 # Define class
 
 class BankAccount:
-    pass
+    def __init__(self, owner: str, balance: float) -> None:
+        self.owner = owner
+        self.balance = balance
 
-peter_account = BankAccount()
+peter_account = BankAccount("Peter Python", 500.0)
 peter_account.owner = "Peter Python"
 peter_account.balance = 500.0
 
@@ -17,3 +19,17 @@ print(peter_account.balance)
 peter_account.balance = 1_000
 
 print(f"{peter_account.owner} new balance: ${peter_account.balance}")
+
+# Construct other objects
+
+jack_account: BankAccount = BankAccount("Jack Sheridan", 5000)
+mark_account: BankAccount = BankAccount("Mark Monroe", 3500)
+
+print(f"{jack_account.owner}: ${jack_account.balance}")
+print(f"{mark_account.owner}: ${mark_account.balance}")
+
+jack_account.balance = 7400
+mark_account.balance += 2000
+
+print(f"New balance({jack_account.owner}): ${jack_account.balance}")
+print(f"New balance({mark_account.owner}): ${mark_account.balance}")
