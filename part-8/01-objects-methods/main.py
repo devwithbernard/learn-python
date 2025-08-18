18648539b21419690190bbaa95921884be1c52c7
Bernard Konan <devwithbernard@gmail.com>
2025-08-18 18:22:48 +0000

    Objects and methods: Group data using dictionary

--- part-8/01-objects-methods/main.py
+++ part-8/01-objects-methods/main.py
@@ -12,3 +12,9 @@ year = 1992
 book = (name, author, year)
 
 print("Name of book:", book[0])
+
+# combine these in dictionary
+new_book = {'name': name, 'author': author, 'year': year}
+
+for key, item in new_book.items():
+    print(f"{key}: {item}")
