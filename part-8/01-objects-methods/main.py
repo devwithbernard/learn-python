6a81a8b4bc9a8c5c0be2e7ce4fa5131c845fb29f
Bernard Konan <devwithbernard@gmail.com>
2025-08-18 18:20:00 +0000

    Objects and methods: Group data using tuple

---
+++ part-8/01-objects-methods/main.py
@@ -0,0 +1,14 @@
+"""
+Objects and Methods
+"""
+
+# Group Data
+
+name = "In Search of lost Typing"
+author = "Marcel Pythons"
+year = 1992
+
+# Combine these in tuple
+book = (name, author, year)
+
+print("Name of book:", book[0])
