fb6863623753476483fa118ec57dfa56022c65f6
Bernard Konan <devwithbernard@gmail.com>
2025-08-31 15:03:19 +0000

    Encapsulation: Example

--- part-9/03-encapsulation/main.py
+++ part-9/03-encapsulation/main.py
@@ -69,4 +69,47 @@ wallet.money = 50
 print("Money after setting a new positive amount:", wallet.money)
 
 wallet.money = -30
-print('Money after setting a negative amount:',wallet.money)
+print('Money after setting a negative amount:', wallet.money)
+
+
+# Example of encapsulation
+
+class Player:
+    def __init__(self, name: str, player_number: int) -> None:
+        self.__name = name
+        self.__player_number = player_number
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        if name != '':
+            self.__name = name
+        if name == '':
+            raise ValueError('The name may not be an empty string')
+        elif len(name) < 4:
+            raise ValueError('The name may not be too short')
+
+
+    @property
+    def player_number(self) -> int:
+        return self.__player_number
+
+    @player_number.setter
+    def player_number(self, player_number: int) -> None:
+        if player_number > 0:
+            self.__player_number = player_number
+        else:
+            raise ValueError('The player number must be a positive integer')
+
+
+player = Player("Betty Ballmer", 10)
+print(player.name)
+print(player.player_number)
+
+player.name = "Buster Ballmer"
+player.player_number = 11
+print(player.name)
+print(player.player_number)
