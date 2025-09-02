add23a7dbab21faf00a6f7bad6b47912abe34964
Bernard Konan <devwithbernard@gmail.com>
2025-09-02 21:01:47 +0000

    Attributes classes:  Class methods

--- part-9/04-class-attributes/main.py
+++ part-9/04-class-attributes/main.py
@@ -104,3 +104,45 @@ pn = PhoneNumber("Peter Pythons", "040 111 1111", "Sweden")
 print(pn)
 print(pn.phone_number)
 print(pn.local_number)
+
+# Class methods
+
+class Registration:
+    def __init__(self, owner: str, make: str, year: int, license_plate: str) -> None:
+        self.__owner = owner
+        self.__make = make
+        self.__year = year
+
+        self.__license_plate = license_plate
+
+    @property
+    def license_plate(self) -> str:
+        return self.__license_plate
+
+    @license_plate.setter
+    def license_plate(self, plate):
+        if Registration.license_plate_valid(plate):
+            self.__license_plate = plate
+        else:
+            raise ValueError("The license plate is not valid")
+
+    # A class method for validating the license plate
+    @classmethod
+    def license_plate_valid(cls, plate: str):
+        if len(plate) < 3 or "-" not in plate:
+            return False
+
+        # Check the beginning and end sections of the plate separately
+        letters, numbers = plate.split("-")
+
+        # the beginning section can have only letters
+        for character in letters:
+            if character.lower() not in "abcdefghijklmnopqrstuvwxyzåäö":
+                return False
+
+        # the end section can have only numbers
+        for character in numbers:
+            if character not in "1234567890":
+                return False
+
+        return True
