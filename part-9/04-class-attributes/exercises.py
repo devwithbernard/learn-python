dc7716b97898514c39a647b265fa350491ccf8a5
Bernard Konan <devwithbernard@gmail.com>
2025-09-03 17:32:23 +0000

    Class attributes: Exercise -> List helper

--- part-9/04-class-attributes/exercises.py
+++ part-9/04-class-attributes/exercises.py
@@ -41,3 +41,48 @@ class City:
 
     def __str__(self) -> str:
         return f"{self.__name} ({self.__post_code})"
+
+
+# TODO: List helper
+
+class ListHelper:
+
+    @classmethod
+    def __helper(cls, items: list):
+        frequency_items = {}
+
+        if items:
+            for item in items:
+                frequency_items[item] = frequency_items.get(item, 0) + 1
+        return frequency_items
+
+    @classmethod
+    def greatest_frequency(cls, items: list):
+        frequency_items = ListHelper.__helper(items)
+
+        if not frequency_items:
+            return None
+
+        max_count = max(frequency_items.values())
+
+        # Return the first item with maximum frequency
+        for item, count in frequency_items.items():
+            if count == max_count:
+                return item
+
+    @classmethod
+    def doubles(cls, items: list):
+        frequency_items = ListHelper.__helper(items)
+
+        values = []
+
+        for item, count in frequency_items.items():
+            if count == 2:
+                values.append(item)
+
+        return ', '.join([str(value) for value in values])
+
+
+numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
+print(ListHelper.greatest_frequency(numbers))
+print(ListHelper.doubles(numbers))
