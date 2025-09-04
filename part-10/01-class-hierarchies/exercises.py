a2a1601e3f0f5ebdfc706fb7685b69534050cbfb
Bernard Konan <devwithbernard@gmail.com>
2025-09-04 15:04:32 +0000

    Class hierarchies: Computer game exercise

--- part-10/01-class-hierarchies/exercises.py
+++ part-10/01-class-hierarchies/exercises.py
@@ -23,3 +23,42 @@ class LaptopComputer(Computer):
 
 laptop = LaptopComputer("NoteBook Pro15", 1500, 2)
 print(laptop)
+
+# TODO: Game Museum
+
+class ComputerGame:
+    def __init__(self, name: str, production_house: str, production_year: int):
+        self.name = name
+        self.studio = production_house
+        self.year = production_year
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.year}) by {self.studio}"
+
+
+class GameWarehouse:
+    def __init__(self):
+        self.games = []
+
+    def add_game(self, game: ComputerGame) -> None:
+        self.games.append(game)
+
+    def list_games(self) -> list[ComputerGame]:
+        return self.games
+
+class GameMuseum(GameWarehouse):
+    def __init__(self):
+        super().__init__()
+
+    def list_games(self) -> list[ComputerGame]:
+        games = [game for game in self.games if game.year < 1990]
+        return games
+
+
+museum = GameMuseum()
+museum.add_game(ComputerGame("Pacman", "Namco", 1980))
+museum.add_game(ComputerGame("GTA 2", "Rockstar", 1999))
+museum.add_game(ComputerGame("Bubble Bobble", "Taito", 1986))
+
+for game in museum.list_games():
+    print(game)
