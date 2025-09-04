e4087ff21ff855742958381b6fa910a9fa469f3a
Bernard Konan <devwithbernard@gmail.com>
2025-09-04 14:00:24 +0000

    Class hierarchies: another inheritance case

--- part-10/01-class-hierarchies/main.py
+++ part-10/01-class-hierarchies/main.py
@@ -37,3 +37,65 @@ print(saul.email)
 tara = Teacher("Tara Teacher", "tara@example.fi", "A123", 2)
 tara.update_email_domain("example.ex")
 print(tara.email)
+
+# Second inheritance
+
+class Book:
+    """
+    This class models a simple book
+    """
+    def __init__(self, name: str, author: str) -> None:
+        self.name = name
+        self.author = author
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.author})"
+
+
+class BookContainer:
+    """This class models a container for books"""
+    def __init__(self) -> None:
+        self.books: list[Book] = []
+
+    def add_book(self, book: Book) -> None:
+        self.books.append(book)
+
+    def list_books(self) -> None:
+        for book in self.books:
+            print(book)
+
+class Bookshelf(BookContainer):
+    """This class models a shelf for books"""
+    def __init__(self) -> None:
+        super().__init__()
+
+    def add_book(self, book: Book, location: int) -> None:
+        self.books.insert(location, book)
+
+
+# Create some books for testing
+b1 = Book("Old Man and the Sea", "Ernest Hemingway")
+b2 = Book("Silent Spring", "Rachel Carson")
+b3 = Book("Pride and Prejudice", "Jane Austen")
+
+# Create a BookContainer and add the books
+container = BookContainer()
+container.add_book(b1)
+container.add_book(b2)
+container.add_book(b3)
+
+# Create a Bookshelf and add the books (always to the beginning)
+shelf = Bookshelf()
+shelf.add_book(b1, 0)
+shelf.add_book(b2, 0)
+shelf.add_book(b3, 0)
+
+
+# Tulostetaan
+print("Container:")
+container.list_books()
+
+print()
+
+print("Shelf:")
+shelf.list_books()
