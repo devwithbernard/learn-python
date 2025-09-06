a634a7f268f7d05c93b6d673b890c08791483f9d
Bernard Konan <devwithbernard@gmail.com>
2025-09-06 22:07:10 +0000

    Develop larger application: Main file

---
+++ part-10/02-larger-application/main.py
@@ -0,0 +1,9 @@
+from app.phone_book import PhoneBookApplication
+
+def main() -> None:
+    application = PhoneBookApplication()
+    application.execute()
+
+
+if __name__ == '__main__':
+    main()
