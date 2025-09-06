a8173fc51fb336436602bfd15755556136363702
Bernard Konan <devwithbernard@gmail.com>
2025-09-06 22:18:40 +0000

    List comprehensions: using list comprehensions to make some calculus

--- part-11/01-list-comprehensions/main.py
+++ part-11/01-list-comprehensions/main.py
@@ -10,3 +10,19 @@ for number in numbers:
     strings.append(str(number))
 
 print(strings)
+
+# Using list comprehensions
+strings = [str(number) for number in numbers]
+print("Using list comprehensions:", strings)
+
+
+def factorials(n):
+    k = 1
+    while n >= 2:
+        k *= n
+        n -= 1
+
+    return k
+
+factorial_numbers = [factorials(number) for number in numbers]
+print(f"Factorial {numbers}: {factorial_numbers}")
