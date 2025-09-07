f04111eec6421ec37bdf8297fa1c6327eed7d3bb
Bernard Konan <devwithbernard@gmail.com>
2025-09-07 15:06:17 +0000

    More comprehensionsPrice difference of cheaper properties

--- part-11/02-more-comprehensions/exercises.py
+++ part-11/02-more-comprehensions/exercises.py
@@ -54,3 +54,30 @@ my_list.add("pineapple", 1)
 print("the shopping list contains at least 8 of the following items:")
 for product in products_in_shopping_list(my_list, 8):
     print(product)
+
+
+# TODO: Price difference of cheaper properties
+
+class RealProperty:
+    def __init__(self, rooms: int, square_metres: int, price_per_sqm: int, description: str):
+        self.rooms = rooms
+        self.square_metres = square_metres
+        self.price_per_sqm = price_per_sqm
+        self.description = description
+
+
+def cheaper_properties(properties: list[RealProperty], property: RealProperty):
+    return [p for p in properties if p.price_per_sqm > property.price_per_sqm]
+
+a1 = RealProperty(1, 16, 5500, "Central studio")
+a2 = RealProperty(2, 38, 4200, "Two bedrooms downtown")
+a3 = RealProperty(3, 78, 2500, "Three bedrooms in the suburbs")
+a4 = RealProperty(6, 215, 500, "Farm in the middle of nowhere")
+a5 = RealProperty(4, 105, 1700, "Loft in a small town")
+a6 = RealProperty(25, 1200, 2500, "Countryside mansion")
+
+properties = [a1, a2, a3, a4, a5, a6]
+
+print(f"cheaper options when compared to {a3.description}:")
+for item in cheaper_properties(properties, a3):
+    print(f"{item.description:35} price difference {item.price_per_sqm - a3.price_per_sqm} euros")
