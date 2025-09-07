66ebabd52a19b238873927c82706bc5e375867a9
Bernard Konan <devwithbernard@gmail.com>
2025-09-07 07:59:46 +0000

    List comprehensions: Rows of stars exercise

--- part-11/01-list-comprehensions/exercises.py
+++ part-11/01-list-comprehensions/exercises.py
@@ -14,3 +14,19 @@ def square_roots(numbers: list[float]) -> list[float]:
 lines = square_roots([1,2,3,4])
 for line in lines:
     print(line)
+
+# TODO: Rows of stars
+def rows_of_stars(numbers: list[int]) -> list[str]:
+    if not numbers:
+        raise ValueError(f'{numbers} is not in correct format')
+    return [ '*' * x for x in numbers]
+
+rows = rows_of_stars([1,2,3,4])
+for row in rows:
+    print(row)
+
+print()
+
+rows = rows_of_stars([4, 3, 2, 1, 10])
+for row in rows:
+    print(row)
