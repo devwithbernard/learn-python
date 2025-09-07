34cdbb471cccdca39316df1ecf8d0d7553ff74b6
Bernard Konan <devwithbernard@gmail.com>
2025-09-07 08:33:36 +0000

    Lists comprehensions: Alternative execution with list comprehensions

--- part-11/01-list-comprehensions/main.py
+++ part-11/01-list-comprehensions/main.py
@@ -36,3 +36,16 @@ print("Even numbers:", even_numbers)
 numbers = [-2, 3, -1, 4, -10, 5, 1]
 factorial_numbers = [(number, factorials(number)) for number in numbers if number >= 0]
 print(factorial_numbers)
+
+# Alternative execution with list comprehensions
+
+numbers = [1, -3, 45, -110, 2, 9, -11]
+abs_values = [number if number >= 0 else -number for number in numbers]
+print("Abs values:", abs_values)
+
+def string_lengths(my_list: list) -> list[int]:
+    return [len(item) if isinstance(item , str) else -1 for item in my_list]
+
+test_list = ["hi", 3, True, "there", -123.344, "toodlepip", 2, False]
+lengths = string_lengths(test_list)
+print(lengths)
