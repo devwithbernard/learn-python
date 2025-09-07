6787017aed7eee4c2076bb8c6dfef0444e85b23e
Bernard Konan <devwithbernard@gmail.com>
2025-09-07 08:08:52 +0000

    List comprehensions: Best exam result

--- part-11/01-list-comprehensions/exercises.py
+++ part-11/01-list-comprehensions/exercises.py
@@ -30,3 +30,28 @@ print()
 rows = rows_of_stars([4, 3, 2, 1, 10])
 for row in rows:
     print(row)
+
+
+# TODO: Best exam result
+class ExamResult:
+    def __init__(self, name: str, grade1: int, grade2: int, grade3: int) -> None:
+        self.name = name
+        self.grade1 = grade1
+        self.grade2 = grade2
+        self.grade3 = grade3
+
+    @property
+    def grades(self):
+        return [self.grade1, self.grade2, self.grade3]
+
+
+def best_results(results: list[ExamResult]) -> dict[str, list[int]]:
+    max_result = max(results, key=lambda result: sum(result.grades))
+    return {max_result.name: max_result.grades}
+
+
+result1 = ExamResult("Peter",5,3,4)
+result2 = ExamResult("Pippa",3,4,1)
+result3 = ExamResult("Paul",2,1,3)
+results = [result1, result2, result3]
+print(best_results(results))
