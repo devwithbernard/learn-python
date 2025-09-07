b1cf02c96766103c048fd088fd259c474a07a303
Bernard Konan <devwithbernard@gmail.com>
2025-09-07 14:54:40 +0000

    More comprehensions: Products in shopping list

--- part-11/02-more-comprehensions/exercises.py
+++ part-11/02-more-comprehensions/exercises.py
@@ -8,3 +8,49 @@ def filter_forbidden(text: str, forbidden: str):
 sentence = "Once! upon, a time: there was a python!??!?!"
 filtered = filter_forbidden(sentence, "!?:,.")
 print(filtered)
+
+# TODO: Products in shopping list
+
+class Product:
+    def __init__(self, name: str, amount: int):
+        self.name = name
+        self.amount = amount
+
+    def __str__(self):
+        return f"Product: {self.name}, ${self.amount:,d.2}"
+
+
+class ShoppingList:
+    def __init__(self):
+        self.products: list[Product] = []
+
+    def add(self, product_name: str, amount: int):
+        product = Product(product_name, amount)
+        self.products.append(product)
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n < len(self.products):
+            product = self.products[self.n]
+            self.n += 1
+            return product
+        else:
+            raise StopIteration
+
+
+def products_in_shopping_list(shopping_list: ShoppingList, amount: int):
+    return [product.name for product in shopping_list if product.amount >= amount]
+
+
+my_list = ShoppingList()
+my_list.add("bananas", 10)
+my_list.add("apples", 5)
+my_list.add("alcohol free beer", 24)
+my_list.add("pineapple", 1)
+
+print("the shopping list contains at least 8 of the following items:")
+for product in products_in_shopping_list(my_list, 8):
+    print(product)
