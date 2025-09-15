30fb426737f6505dc4c8b6db71e998a09e7f69e5
Bernard Konan <devwithbernard@gmail.com>
2025-09-15 21:39:01 +0000

    Recursion: Exercise -> Add numbers to a list

---
+++ part-11/03-recursion/exercises.py
@@ -0,0 +1,17 @@
+"""
+Exercises: Recursion
+"""
+
+
+# TODO: Add numbers to a list
+
+def add_numbers_to_list(numbers: list):
+    if len(numbers) % 5 != 0:
+        max_number = max(numbers)
+        numbers.append(max_number + 1)
+        add_numbers_to_list(numbers)
+
+
+numbers = [1, 3, 4, 5, 10, 11]
+add_numbers_to_list(numbers)
+print(numbers)
