a7294a2dc8a649b00590a99450f595c41d1d96a4
Bernard Konan <devwithbernard@gmail.com>
2025-09-15 08:05:48 +0000

    Recursion: write some recursive functions

--- part-11/03-recursion/main.py
+++ part-11/03-recursion/main.py
@@ -12,3 +12,28 @@ def hello_many_times(name: str, times: int) -> None:
 
 
 hello_many_times("Bernard", 5)
+
+# Recursion
+def fill_list(numbers: list):
+    """ if the length of the list is less than 10, add items to the list """
+    if len(numbers) < 10:
+        numbers.append(0)
+        fill_list(numbers)
+
+test_list = [1, 2, 3, 4]
+fill_list(test_list)
+print(test_list)
+
+
+def fact(number: int) -> int:
+    if number < 0:
+        raise ValueError("the number must be a positive number")
+
+    if number == 0 or number == 1:
+        return 1
+
+    return number * fact(number -1)
+
+
+number = int(input("Enter a number: "))
+print(f"{number}! = {fact(number)}")
