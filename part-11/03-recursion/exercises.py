3c7cdd4c68a60f1c97b46e6d9a34860e85010ffb
Bernard Konan <devwithbernard@gmail.com>
2025-09-15 21:57:48 +0000

    Recursion: Fibonacci and recursion sum

--- part-11/03-recursion/exercises.py
+++ part-11/03-recursion/exercises.py
@@ -15,3 +15,13 @@ def add_numbers_to_list(numbers: list):
 numbers = [1, 3, 4, 5, 10, 11]
 add_numbers_to_list(numbers)
 print(numbers)
+
+
+# TODO: Recursive sum
+def recursive_sum(number: int):
+    if number <= 1:
+        return number
+    return number + recursive_sum(number - 1)
+
+
+print("sum from 1 to 20:", recursive_sum(20))
