0e3af1315d79ef06c99c2c3126582899f1f8228a
Bernard Konan <devwithbernard@gmail.com>
2025-09-16 17:13:00 +0000

    Recursion: Balance all the brackets

--- part-11/03-recursion/exercises.py
+++ part-11/03-recursion/exercises.py
@@ -25,3 +25,36 @@ def recursive_sum(number: int):
 
 
 print("sum from 1 to 20:", recursive_sum(20))
+
+# TODO: Balance all the brackets
+
+def balanced_brackets(my_string: str) -> bool:
+    filtered = "".join([ch for ch in my_string if ch in "()[]"])
+
+    # Base case: empty string
+    if not filtered:
+        return True
+
+    # If the length has odd length, it can't be balanced
+    if len(filtered) % 2 != 0:
+        return False
+
+    # If we find "()" or "[]", remove it and recurse
+    if "()" in filtered:
+        return balanced_brackets(filtered.replace("()", "", 1))
+
+    if "[]" in filtered:
+        return balanced_brackets(filtered.replace("[]", "", 1))
+
+    # Not valid pairs found -> not balanced
+    return False
+
+# Tests
+print(balanced_brackets("(((())))"))        # True
+print(balanced_brackets("()())"))           # False
+print(balanced_brackets(")()"))             # False
+print(balanced_brackets("()(())"))          # True
+print(balanced_brackets("[()]"))            # True
+print(balanced_brackets("[(])"))            # False
+print(balanced_brackets("([][])"))          # True
+print(balanced_brackets("(hello [world])")) # True
