bba06624231680c7e6621924b1f0a641323b5f96
Bernard Konan <devwithbernard@gmail.com>
2025-09-17 13:56:12 +0000

    Functions as arguments: Climbing route: sort by length

--- part-12/01-functions-as-arguments/exercises.py
+++ part-12/01-functions-as-arguments/exercises.py
@@ -24,3 +24,32 @@ shows = [{"name": "Dexter", "rating": 8.6, "seasons": 9}, {"name": "Friends", "r
 
 for show in sort_by_seasons(shows):
     print(f"{show['name']} {show['seasons']} seasons")
+
+
+# TODO: Climbing Route
+
+class ClimbingRoute:
+    def __init__(self, name: str, length: float, grade: str):
+        self.name = name
+        self.length = length
+        self.grade = grade
+
+    def __str__(self):
+        return f"{self.name}, length {self.length} metres, grade {self.grade}"
+
+
+def sort_by_length(routes: list[ClimbingRoute]):
+    def by_length(route: ClimbingRoute):
+        return route.length
+
+    return sorted(routes, key=by_length, reverse=True)
+
+r1 = ClimbingRoute("Edge", 38, "6A+")
+r2 = ClimbingRoute("Smooth operator", 11, "7A")
+r3 = ClimbingRoute("Synchro", 14, "8C+")
+r4 = ClimbingRoute("Small steps", 12, "6A+")
+
+routes = [r1, r2, r3, r4]
+
+for route in sort_by_length(routes):
+    print(route)
