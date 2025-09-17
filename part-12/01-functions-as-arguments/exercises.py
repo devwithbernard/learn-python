05278dbc64a0ee872087b306c967ebfc8d6c5774
Bernard Konan <devwithbernard@gmail.com>
2025-09-17 15:07:00 +0000

    Lambda functions: find ball player based on some criteria

--- part-12/01-functions-as-arguments/exercises.py
+++ part-12/01-functions-as-arguments/exercises.py
@@ -44,6 +44,7 @@ def sort_by_length(routes: list[ClimbingRoute]):
 
     return sorted(routes, key=by_length, reverse=True)
 
+
 r1 = ClimbingRoute("Edge", 38, "6A+")
 r2 = ClimbingRoute("Smooth operator", 11, "7A")
 r3 = ClimbingRoute("Synchro", 14, "8C+")
@@ -53,3 +54,48 @@ routes = [r1, r2, r3, r4]
 
 for route in sort_by_length(routes):
     print(route)
+
+
+# TODO: BallPlayers
+
+class BallPlayer:
+    """ This class models a single ball player """
+
+    def __init__(self, name: str, shirt_number: int, goals: int, assists: int, minutes: int):
+        self.name = name
+        self.number = shirt_number
+        self.goals = goals
+        self.assists = assists
+        self.minutes = minutes
+
+    def __str__(self):
+        return (f"BallPlayer(name={self.name}, number={self.number}, goals={self.goals}, passes={self.assists}, "
+                f"minutes={self.minutes})")
+
+
+def most_goals(ball_players: list[BallPlayer]):
+    player = max(ball_players, key=lambda ball_player: ball_player.goals)
+    return player.name
+
+
+def most_points(ball_players: list[BallPlayer]) -> tuple[str, int]:
+    player = max(ball_players, key=lambda ball_player: ball_player.goals + ball_player.assists)
+    return player.name, player.number
+
+
+def least_minutes(ball_players: list[BallPlayer]):
+    return min(ball_players, key=lambda ball_player: ball_player.minutes)
+
+
+print()
+
+player1 = BallPlayer("Archie Bonkers", 13, 5, 12, 46)
+player2 = BallPlayer("Speedy Tickets", 7, 2, 26, 55)
+player3 = BallPlayer("Cruella De Hill", 9, 1, 32, 26)
+player4 = BallPlayer("Devilled Tasmanian", 12, 1, 11, 41)
+player5 = BallPlayer("Donald Quack", 4, 3, 9, 12)
+
+team = [player1, player2, player3, player4, player5]
+print(most_goals(team))
+print(most_points(team))
+print(least_minutes(team))
