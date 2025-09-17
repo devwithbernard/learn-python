0c257957cbb9e038a30cbad6fa45b80ebff2ecf6
Bernard Konan <devwithbernard@gmail.com>
2025-09-17 13:28:48 +0000

    Functions as arguments: Sort collections on objects

--- part-12/01-functions-as-arguments/main.py
+++ part-12/01-functions-as-arguments/main.py
@@ -41,3 +41,44 @@ for person in persons_order_by_age:
     print(f"{person[0]}: {person[1]} years old.")
 
 print()
+
+
+# Sorting collections of your own objects
+
+class Student:
+    def __init__(self, name: str, student_id: str, credits: int):
+        self.__student_id = student_id
+        self.name = name
+        self.credits = credits
+
+    def __str__(self):
+        return f"{self.name} ({self.student_id}), {self.credits} cr."
+
+    @property
+    def student_id(self):
+        return self.__student_id
+
+
+def sort_by_id(student: Student):
+    return student.student_id
+
+
+def sort_by_credits(student: Student):
+    return student.credits
+
+
+o1 = Student("Archie", "a123", 220)
+o2 = Student("Marvin", "m321", 210)
+o3 = Student("Anna", "a999", 131)
+
+students = [o1, o2, o3]
+
+print("Sort by id:")
+for student in sorted(students, key=sort_by_id):
+    print(student)
+
+print()
+
+print("Sort by credits:")
+for student in sorted(students, key=sort_by_credits):
+    print(student)
