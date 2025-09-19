b5402c89ae031ba9be8ef48ceb7724a29e09ad08
Bernard Konan <devwithbernard@gmail.com>
2025-09-19 14:48:34 +0000

    Decorators: introduction

---
+++ part-12/03-decorators/main.py
@@ -0,0 +1,17 @@
+"""
+Decorators
+"""
+import logging
+
+def decorator(func):
+    def wrapper():
+        print("Before calling the function")
+        func()
+        print("After calling the function")
+    return wrapper
+
+@decorator
+def greet():
+    print("Hello word")
+
+greet()
