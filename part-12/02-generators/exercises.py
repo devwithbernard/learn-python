f310e258eacc69f95cce0d880c1b234ad9b49148
Bernard Konan <devwithbernard@gmail.com>
2025-09-19 08:44:49 +0000

    Generators: prime numbers

--- part-12/02-generators/exercises.py
+++ part-12/02-generators/exercises.py
@@ -16,3 +16,29 @@ numbers = even_numbers(11, 21)
 
 for number in numbers:
     print(number)
+
+# TODO: Prime numbers
+
+def prime_numbers():
+    def is_prime(number: int):
+        if number < 2:
+            return False
+
+        for i in range(2, number):
+            if number % i == 0:
+                return False
+
+        return True
+
+    number = 2
+
+    while True:
+        if is_prime(number):
+            yield number
+        number += 1
+
+numbers = prime_numbers()
+for i in range(10):
+    print(next(numbers))
+
+
