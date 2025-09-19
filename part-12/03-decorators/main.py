ad8748c87baa4057f61cec4392ba1176af96644c
Bernard Konan <devwithbernard@gmail.com>
2025-09-19 15:52:16 +0000

    Decorators: decorate a function with parameters

--- part-12/03-decorators/main.py
+++ part-12/03-decorators/main.py
@@ -1,7 +1,6 @@
 """
 Decorators
 """
-import logging
 
 def decorator(func):
     def wrapper():
@@ -15,3 +14,20 @@ def greet():
     print("Hello word")
 
 greet()
+
+# Decorator with parameters
+def decorator_name(function):
+    def wrapper(*args, **kwargs):
+        print("Before execution")
+        result = function(*args, **kwargs)
+        print("After execution")
+        return result
+    return wrapper
+
+
+@decorator_name
+def add(a, b):
+    return a + b
+
+
+print(add(10, 5))
